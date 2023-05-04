eb81cea92c460b5727aa51f3eb0698de758a3f40
Andrew S <samum_94@mail.ru>
2023-05-04 23:26:39 +0300

    Autotest created

--- src/test/java/ru/netology/AppFormTest.java
+++ src/test/java/ru/netology/AppFormTest.java
@@ -1,13 +1,13 @@
 
 package ru.netology;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppFormTest {
     private WebDriver driver;
@@ -34,7 +34,15 @@ public class AppFormTest {
 
     @Test
     void shouldTestSomething() {
-        driver.get("http://localhost:9999");
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79021234567");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+
     }
 }
 
